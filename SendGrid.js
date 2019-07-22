74cbabf0a3b29f57d19b44499daac095dbaf9393
Andres Rojas Ugarte <sergio.rojas0@walmart.com>
2019-07-22 07:52:59 -0400

    Segundo envio

--- SendGrid.js
+++ SendGrid.js
@@ -1,2 +1,15 @@
 //  Prueba de usod e SendGRid via API
 
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail');
+//sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey("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");
+const msg = {
+  to: 'hugo.collao0@walmart.com',
+  from: 'CESI360@walmart.com<CESI360@hotmail.com>',
+  subject: 'CESI360 le recuerda..... ',
+  text: 'CESI360 le recuerda que tiene tareas pendentes en e sistema',
+  html: '<h2><strong>Tiene APROBACIONES pendientes en CESI360</strong></h2><P>Por favor ingrese al siguiente link para proceder&nbsp <a href=#>Haga click aqui</a></P>',
+};
+sgMail.send(msg).then(x=>{console.log("listo ", x)}).catch(e => console.log("E-STACK: ",e.stack));
